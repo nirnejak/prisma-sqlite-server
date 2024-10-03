5231b49c0485fb7fa2f04778d43c83ece1495c62
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-10-04 00:58:08 +0530

    User Create and Get routes

--- routes/userRoute.ts
+++ routes/userRoute.ts
@@ -1,19 +1,32 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 export const userRoutes = async (fastify: FastifyInstance) => {
   fastify.post("/", {
     handler: async (
-      request: FastifyRequest<{
-        Body: {
-          name: string;
-          age: number;
-        };
-      }>,
+      request: FastifyRequest<{ Body: { name: string; email: string } }>,
       reply: FastifyReply
     ) => {
-      const body = request.body;
+      const { name, email } = request.body;
+      const prisma = new PrismaClient();
 
-      return reply.code(201).send(body);
+      const user = await prisma.user.create({
+        data: { name, email },
+      });
+      prisma.$disconnect();
+
+      return reply.code(201).send(user);
+    },
+  });
+
+  fastify.get("/", {
+    handler: async (request: FastifyRequest, reply: FastifyReply) => {
+      const prisma = new PrismaClient();
+
+      const allUsers = await prisma.user.findMany();
+      prisma.$disconnect();
+
+      return reply.code(201).send({ users: allUsers });
     },
   });
 };
